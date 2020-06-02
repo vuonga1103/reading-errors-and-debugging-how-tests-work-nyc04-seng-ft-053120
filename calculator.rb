dbc99aa0f8e26ca374119c1c931c85f223557a8f
Anh Vuong <vuonga1103@yahoo.com>
2020-06-02 13:54:34 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,4 +5,4 @@ second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-quotient = first_number / second_number
+quotient = first_number.to_f / second_number
